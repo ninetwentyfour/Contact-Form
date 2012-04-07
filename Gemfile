4ab95f577deeae64d02232b407d7c06c5eae4d9d
Travis Berry <travisberry@travisberry.com>
2012-04-07 17:20:34 -0600

    initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source :rubygems
+
+gem "mustache"
+gem "sinatra"
+gem "shotgun"
+gem "json"
+gem "ruby-mp3info"
+gem "newrelic_rpm"
+gem "pony"
---
+++ contact_form.rb
@@ -0,0 +1,119 @@
+# See blog post at http://vitobotta.com/sinatra-contact-form-jekyll/
+
+%w(rubygems sinatra liquid active_support/secure_random resolv open-uri haml pony).each{ |g| require g }
+
+# APP_ROOT = File.join(File.dirname(__FILE__), '..')
+# 
+# set :root,  APP_ROOT
+# set :views, File.join(APP_ROOT, "_layouts")
+
+not_found do
+  status 404
+  not_found_template = File.join(APP_ROOT, "public", "404.html")
+  File.exists?(not_found_template) ? File.read(not_found_template) : "Oops, the page you are looking for does not exist :("
+end
+
+def valid_email?(email)
+  if email =~ /^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$/
+    domain = email.match(/\@(.+)/)[1]
+    Resolv::DNS.open do |dns|
+        @mx = dns.getresources(domain, Resolv::DNS::Resource::IN::MX)
+    end
+    @mx.size > 0 ? true : false
+  else
+    false
+  end
+end
+
+def valid_url? url
+  return true if url == "http://"
+  !(url =~ /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix).nil?
+end
+
+def given? field
+  !field.empty?
+end
+
+def validate params
+  errors = {}
+  
+  [:name, :email, :message].each{|key| params[key] = (params[key] || "").strip }
+  
+  errors[:name]    = "This field is required" unless given? params[:name]
+  
+  if given? params[:email]
+    errors[:email]   = "Please enter a valid email address" unless valid_email? params[:email]
+  else
+    errors[:email]   = "This field is required"
+  end
+  
+  errors[:message] = "This field is required" unless given? params[:message]
+  
+  
+  errors
+end
+
+def send_email params, ip_address
+  email_template = <<-EOS
+  
+  
+  
+  Sent via http://www.travisberry.com/contact/ 
+  When:          {{ when }}  
+  IP address:    {{ ip_address }}
+  
+  Your name:     {{ name }}
+  Email:         {{ email }}
+  
+  Message:       
+  
+  {{ message }}
+  
+  EOS
+  
+  body = Liquid::Template.parse(email_template).render  "name"       => params[:name], 
+                                                        "email"      => params[:email], 
+                                                        "website"    => params[:website], 
+                                                        "message"    => params[:message], 
+                                                        "when"       => Time.now.strftime("%b %e, %Y %H:%M:%S %Z"), 
+                                                        "ip_address" => ip_address
+  
+  Pony.options = {
+    :via => :smtp,
+    :via_options => {
+      :address => 'smtp.sendgrid.net',
+      :port => '587',
+      :domain => 'heroku.com',
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
+  }
+  
+  Pony.mail(:to => "contact@travisberry.com", :from => params[:email], :subject => "A comment from #{params[:name]}", :body => body)
+end
+
+
+get '/contact-form/?' do
+  @errors, @values, @sent = {}, {}, false
+  haml :contact_form
+end
+
+post '/contact-form/?' do
+  @errors     = validate(params)
+  @values     = params
+
+  if @errors.empty?
+    begin
+      send_email(params, @env['REMOTE_ADDR']) 
+      @sent = true
+    
+    rescue Exception => e
+      puts e
+      @failure = "Ooops, it looks like something went wrong while attempting to send your email. Mind trying again now or later? :)"
+    end
+  end
+  
+  haml :contact_form
+end
